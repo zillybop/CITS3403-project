99e30c1869515f806dad314dc5d3b699c416f4f1
Alec Wollaston <alecwollaston@gmail.com>
2025-04-23 22:35:36 +0800

    feat(login): created LoginForm class to handle logins

--- app/forms.py
+++ app/forms.py
@@ -1,3 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, BooleanField, SubmitField
+from wtforms import StringField, IntegerField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = StringField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Log In')
