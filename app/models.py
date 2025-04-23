0f312fae9deaa824b7752b28b7634c98542197d6
Alec Wollaston <alecwollaston@gmail.com>
2025-04-23 23:11:34 +0800

    feat(database/login): updated User model to record password hash, with set/get methods

--- app/models.py
+++ app/models.py
@@ -1,12 +1,19 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from app import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(20), unique=False, nullable=False)
+    password_hash = db.Column(db.String(20), unique=False, nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
     
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 class ImageOutput(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     filename = db.Column(db.String(150), nullable=False)
