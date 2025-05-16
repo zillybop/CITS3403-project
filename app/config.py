4fdddc02cd3e5ac23fb00c2b73da916bb5db4da2
Alec Wollaston <alecwollaston@gmail.com>
2025-05-16 21:46:12 +0800

    Restore all project files to last known working state

--- app/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-default_database_location = 'sqlite:///' + os.path.join(basedir, 'app.db')
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or default_database_location
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "fallback-dev-key"
--- app/static/js/script.js
+++
@@ -1,6 +0,0 @@
-// JavaScript will go here
-let cvReady = false;
-function onOpenCvReady() {
-    console.log('OpenCV.js is ready!');
-    cvReady = true;
-}
